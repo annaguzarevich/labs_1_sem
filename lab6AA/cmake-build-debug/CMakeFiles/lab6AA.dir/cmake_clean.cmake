f4e6f6282607d2d07d415f85d65192c4d79751fb
annaguzarevich <90004596+annaguzarevich@users.noreply.github.com>
2021-12-28 11:29:35 +0300

    Add files via upload

---
+++ lab6AA/cmake-build-debug/CMakeFiles/lab6AA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab6AA.dir/main.cpp.obj"
+  "lab6AA.exe"
+  "lab6AA.exe.manifest"
+  "lab6AA.pdb"
+  "liblab6AA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab6AA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
